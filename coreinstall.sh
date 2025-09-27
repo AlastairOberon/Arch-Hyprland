97f58b30ebecc49485579b0dacb3a03713fa7bdd
AlastairOberon <alastair.gba@gmail.com>
2025-09-27 23:28:49 +0530

    -

--- coreinstall.sh
+++ coreinstall.sh
@@ -3,7 +3,7 @@ set -e
 echo "Initializing Critical System Components..."
 mkdir -p "$HOME/.config/hypr"
 mkdir -p "$HOME/.config/hypr/HyprLand_Scripts"
-sudo cp --noconfirm "$HOME/Arch-Hyprland/HyprLand_ConfigFiles/hyprland.conf" "$HOME/.config/hypr/hyprland.conf"
+sudo cp "$HOME/Arch-Hyprland/HyprLand_ConfigFiles/hyprland.conf" "$HOME/.config/hypr/hyprland.conf"
 sudo pacman -S --noconfirm xdg-desktop-portal-hyprland
 sudo pacman -S --noconfirm polkit-kde-agent
 sudo pacman -S --noconfirm qt5-wayland qt6-wayland
@@ -11,7 +11,7 @@ sudo pacman -S --noconfirm dunst
 sudo pacman -S --noconfirm brightnessctl
 sudo pacman -S --noconfirm pamixer
 sudo pacman -S --noconfirm jq
-sudo cp --noconfirm "$HOME/Arch-Hyprland/HyprLand_Scripts/hypr_ToggleOpacity.sh" "$HOME/.config/hypr/HyprLand_Scripts/hypr_ToggleOpacity.sh"
+sudo cp "$HOME/Arch-Hyprland/HyprLand_Scripts/hypr_ToggleOpacity.sh" "$HOME/.config/hypr/HyprLand_Scripts/hypr_ToggleOpacity.sh"
 
 
 sudo reboot now
