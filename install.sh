315187190f68af4cc209291a8ecfd6ca5a240f97
AlastairOberon <alastair.gba@gmail.com>
2025-10-04 07:35:18 +0530

    -

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 set -e
 
+PINK='\033[1;35m'    # Bright magenta looks like pink
+PURPLE='\033[0;35m'  # Normal purple/magenta
+YELLOW='\033[1;33m'  # Bright yellow
+RED='\033[0;31m'     # Red (already in your script)
+RESET='\033[0m'      # Reset color
+
+
+echo -e "${YELLOW}Updating Files...${RESET}"
 sudo pacman -Syu --noconfirm 
 
+clear
+
+echo -e "${YELLOW}Some Essentials...${RESET}"
 sudo pacman -S --noconfirm vim git wget curl htop 
 
 clear 
@@ -22,6 +33,7 @@ else
 
     case "$choice" in
         1)
+            echo -e "${YELLOW}Installing and Building Paru...${RESET}"
             git clone https://aur.archlinux.org/paru.git
             cd paru || exit
             makepkg -si --noconfirm
@@ -29,6 +41,7 @@ else
             rm -rf paru
             ;;
         2)
+            echo -e "${YELLOW}Installing and Building Yay...${RESET}"
             git clone https://aur.archlinux.org/yay.git
             cd yay || exit
             makepkg -si --noconfirm
@@ -52,26 +65,31 @@ fi
 
 clear
 
-sudo pacman -S --noconfirm pipewire wireplumber
+echo -e "${YELLOW}Installing and Initializing Audio Setup${RESET}"
+sudo pacman -S --noconfirm pipewire wireplumber blueman
 
 clear
 
+echo -e "${YELLOW}Installing and Initializing Fonts${RESET}"
 sudo pacman -S --noconfirm ttf-cascadia-code-nerd ttf-cascadia-mono-nerd ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-firacode-nerd ttf-iosevka-nerd ttf-iosevkaterm-nerd ttf-jetbrains-mono-nerd ttf-jetbrains-mono ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono
-$AUR_HELPER -S --noconfirm ttf-nunito
+$AUR_HELPER -S --noconfirm ttf-nunito-sans
 
 clear
 
+echo -e "${YELLOW}Installing and Initializing Login Manager${RESET}"
 sudo pacman -S --noconfirm sddm
 systemctl enable sddm.service --now
 
 
 clear
 
+echo -e "${YELLOW}Installing Kitty${RESET}"
 sudo pacman -S --noconfirm kitty 
 
 clear
 
-$AUR_HELPER -S --noconfirm visual-studio-code-bin
+echo -e "${YELLOW}Installing and Initializing IEDs${RESET}"
+$AUR_HELPER -S --noconfirm vscodium
 sudo pacman -S --noconfirm nano
 
 clear
@@ -92,6 +110,7 @@ mkdir -p "$HOME/.config/hypr/hyprland_hcripts"
 sudo cp "$HOME/Arch-Hyprland/hyprland_configfiles/hyprland.conf" "$HOME/.config/hypr/hyprland.conf"
 sudo cp "$HOME/Arch-Hyprland/hyprland_configfiles/hyprlock.conf" "$HOME/.config/hypr/hyprlock.conf"
 sudo cp "$HOME/Arch-Hyprland/hyprland_configfiles/hypridle.conf" "$HOME/.config/hypr/hypridle.conf"
+sudo cp "$HOME/Arch-Hyprland/hyprland_configfiles/hyprpaper.conf" "$HOME/.config/hypr/hyprpaper.conf"
 $AUR_HELPER -S --noconfirm wlogout
 
 mkdir -p "$HOME/.config/wlogout"
@@ -104,7 +123,8 @@ clear
 sudo pacman -S --noconfirm rofi
 mkdir -p "$HOME/.config/rofi"
 sudo cp "$HOME/Arch-Hyprland/rofi/config.rasi" "$HOME/.config/rofi/config.rasi"
-#rofi -dump-config > ~/.config/rofi/config.rasi
+
+$AUR_HELPER -S --noconfirm eww-git
 
 
 clear
@@ -120,6 +140,27 @@ clear
 $AUR_HELPER -S --noconfirm swww waypaper
 mkdir -p "$HOME/Pictures/wallpapers"
 sudo cp -r "$HOME/Arch-Hyprland/wallpapers" "$HOME/Pictures/wallpapers"
+
+echo "Do you want to install Zen Browser?"
+echo "1) Yes (zen-browser-bin)"
+echo "2) Skip"
+read -rp "Enter choice [1-2]: " zen_choice
+
+case "$zen_choice" in
+    1)
+        if [ -n "$AUR_HELPER" ]; then
+            $AUR_HELPER -S --noconfirm zen-browser-bin
+        else
+            echo -e "${RED}No AUR helper found. Cannot install via AUR.${RESET}"
+        fi
+        ;;
+    2)
+        echo "Skipping Zen Browser installation."
+        ;;
+    *)
+        echo -e "${RED}Invalid choice. Skipping Zen Browser installation.${RESET}"
+        ;;
+esac
 
 echo "Theme Picker..."
 sudo pacman -S --noconfirm nwg-look
